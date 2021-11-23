5546b88940f385440bf8fafdabf0900f8e1a08f2
Joel Beckum <joelbeckum@gmail.com>
2021-11-23 15:42:05 -0600

    Added basic button submission

--- WpfDemo/MainWindow.xaml.cs
+++ WpfDemo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace WpfDemo
         {
             InitializeComponent();
         }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Hello { firstNameText.Text }!");
+        }
     }
 }
